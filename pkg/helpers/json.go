03d478f790c2d1d4fffdf361aeb250936ce070cc
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-04-27 13:45:46 +0100

    Add helper function to decode and validate data

--- pkg/helpers/json.go
+++ pkg/helpers/json.go
@@ -1,10 +1,16 @@
 package helpers
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type Validator interface {
+	Valid(ctx context.Context) (problems map[string]string)
+}
 
 func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	data, err := json.MarshalIndent(payload, "", "   ")
@@ -19,4 +25,18 @@ func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Write(data)
 
 	return
+}
+
+func DecodeJSON[T Validator](r *http.Request) (T, map[string]string, error) {
+	var v T
+
+	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+		return v, nil, fmt.Errorf("error decoding JSON: %w", err)
+	}
+
+	if problems := v.Valid(r.Context()); len(problems) != 0 {
+		return v, problems, fmt.Errorf("invalid %T: %d problems", v, len(problems))
+	}
+
+	return v, nil, nil
 }
