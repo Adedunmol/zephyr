2fe5df5b69dc35588d9327dc5a9bba8873f5b31f
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-04-27 18:36:16 +0100

    Update errors

--- pkg/helpers/json.go
+++ pkg/helpers/json.go
@@ -3,7 +3,6 @@ package helpers
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,6 +13,7 @@ type Validator interface {
 }
 
 var ErrValidation error
+var ErrDecode error
 
 func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	data, err := json.MarshalIndent(payload, "", "   ")
@@ -34,11 +34,12 @@ func DecodeJSON[T Validator](r *http.Request) (T, map[string]string, error) {
 	var v T
 
 	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
-		return v, nil, fmt.Errorf("error decoding JSON: %w", err)
+		ErrDecode = fmt.Errorf("error decoding JSON: %w", err)
+		return v, nil, ErrDecode
 	}
 
 	if problems := v.Valid(r.Context()); len(problems) != 0 {
-		ErrValidation = errors.New(fmt.Sprintf("invalid %T: %d problem(s)", v, len(problems)))
+		ErrValidation = fmt.Errorf("invalid %T: %d problem(s)", v, len(problems))
 		return v, problems, ErrValidation
 	}
 
