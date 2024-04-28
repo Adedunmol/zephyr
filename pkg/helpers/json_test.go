5a6fc82eb6969ffab0660ebd59905e2e8ab99e1b
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-04-28 15:56:12 +0100

    Add test for validating with the validator package

--- pkg/helpers/json_test.go
+++ pkg/helpers/json_test.go
@@ -7,6 +7,8 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"github.com/go-playground/validator/v10"
 )
 
 var mux *http.ServeMux
@@ -32,6 +34,28 @@ func (r *CreateUser) Valid(ctx context.Context) (problems map[string]string) {
 	return problems
 }
 
+type CreateUserAdv struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+func (r *CreateUserAdv) Valid(ctx context.Context) (problems map[string]string) {
+	problems = make(map[string]string)
+
+	v := validator.New(validator.WithRequiredStructEnabled())
+
+	if err := v.Struct(r); err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			field := err.Field()
+			message := err.Tag()
+
+			problems[field] = message
+		}
+	}
+
+	return problems
+}
+
 func mockServer() (*httptest.Server, *http.ServeMux) {
 	mux = http.NewServeMux()
 
@@ -46,11 +70,23 @@ func mockServer() (*httptest.Server, *http.ServeMux) {
 		RespondWithJSON(w, http.StatusOK, payload)
 	})
 
-	mux.HandleFunc("/decode-json", func(w http.ResponseWriter, r *http.Request) {
-		data, _, err := DecodeJSON[*CreateUser](r)
+	mux.HandleFunc("/basic-decode-json", func(w http.ResponseWriter, r *http.Request) {
+		data, problems, err := DecodeJSON[*CreateUser](r)
 
 		if err != nil && err == ErrValidation {
-			RespondWithJSON(w, http.StatusUnprocessableEntity, err)
+			RespondWithJSON(w, http.StatusUnprocessableEntity, problems)
+			return
+		}
+
+		RespondWithJSON(w, http.StatusOK, data)
+	})
+
+	mux.HandleFunc("/advanced-decode-json", func(w http.ResponseWriter, r *http.Request) {
+		data, problems, err := DecodeJSON[*CreateUserAdv](r)
+
+		if err != nil && err == ErrValidation {
+			RespondWithJSON(w, http.StatusUnprocessableEntity, problems)
+			return
 		}
 
 		RespondWithJSON(w, http.StatusOK, data)
@@ -126,7 +162,7 @@ func TestDecodeJSON(t *testing.T) {
 			}
 			`
 
-			req, err := http.NewRequest(http.MethodPost, server.URL+"/decode-json", strings.NewReader(reqBody))
+			req, err := http.NewRequest(http.MethodPost, server.URL+"/basic-decode-json", strings.NewReader(reqBody))
 
 			if err != nil {
 				t.Fatal("\t\tShould be able to create the POST call", ballotX, err)
@@ -153,7 +189,7 @@ func TestDecodeJSON(t *testing.T) {
 			}
 			`
 
-			req, err := http.NewRequest(http.MethodPost, server.URL+"/decode-json", strings.NewReader(reqBody))
+			req, err := http.NewRequest(http.MethodPost, server.URL+"/basic-decode-json", strings.NewReader(reqBody))
 
 			if err != nil {
 				t.Fatal("\t\tShould be able to create the POST call", ballotX, err)
@@ -169,6 +205,70 @@ func TestDecodeJSON(t *testing.T) {
 			}
 
 			t.Logf("\t\tShould receive a %d status code. %v", statusCode, checkMark)
+		}
+	}
+}
+
+func TestAdvancedDecodeJSON(t *testing.T) {
+	statusCode := http.StatusOK
+
+	server, mux := mockServer()
+	defer server.Close()
+
+	t.Log("Given the need to test decoding JSON.")
+	{
+		t.Log("When checking for a passing validation")
+		{
+			reqBody := `
+			{
+				"username": "Adedunmola", "password": "password123"
+			}
+			`
+
+			req, err := http.NewRequest(http.MethodPost, server.URL+"/advanced-decode-json", strings.NewReader(reqBody))
+
+			if err != nil {
+				t.Fatal("\t\tShould be able to create the POST call", ballotX, err)
+			}
+			t.Log("\t\tShould be able to create the POST call.", checkMark)
+
+			rw := httptest.NewRecorder()
+			mux.ServeHTTP(rw, req)
+
+			if rw.Code != statusCode {
+				t.Log(rw.Body)
+				t.Errorf("\t\tShould receive a %d status code, but got %v. %v", statusCode, rw.Code, ballotX)
+			}
+
+			t.Logf("\t\tShould receive a %d status code. %v", statusCode, checkMark)
+		}
+
+		statusCode = http.StatusUnprocessableEntity
+		t.Log("When checking for a failing validation")
+		{
+			reqBody := `
+			{
+				"username": "Adedunmola", "password": ""
+			}
+			`
+
+			req, err := http.NewRequest(http.MethodPost, server.URL+"/advanced-decode-json", strings.NewReader(reqBody))
+
+			if err != nil {
+				t.Fatal("\t\tShould be able to create the POST call", ballotX, err)
+			}
+			t.Log("\t\tShould be able to create the POST call.", checkMark)
+
+			rw := httptest.NewRecorder()
+			mux.ServeHTTP(rw, req)
+
+			if rw.Code != statusCode {
+				t.Log(rw.Body)
+				t.Errorf("\t\tShould receive a %d status code, but got %v. %v", statusCode, rw.Code, ballotX)
+			}
+
+			t.Logf("\t\tShould receive a %d status code. %v", statusCode, checkMark)
+			t.Log(rw.Body)
 		}
 	}
 }
